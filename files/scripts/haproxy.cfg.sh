6dcea4fd28ef5b9a76675060160904864d76e0a3
Les Begnaud <lbegnaud@radersolutions.com>
2019-10-04 09:30:02 -0500

    stdout logging

--- files/scripts/haproxy.cfg.sh
+++ files/scripts/haproxy.cfg.sh
@@ -16,7 +16,7 @@ USAGE
 function header {
   cat <<EOF
 global
-        log stdout  local0  info
+        log /dev/stdout  local0  info
         #log /dev/log    local0
         #log /dev/log    local1 notice
         chroot /tmp
